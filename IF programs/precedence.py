8113591fd6ab37b39659b4a69354130f70db3984
suresh chandra sekar <sureshkavitha1997@gmail.com>
2018-11-19 20:28:31 +0530

    Update precedence.py

--- IF programs/precedence.py
+++ IF programs/precedence.py
@@ -1,4 +1,7 @@
-
+"""
+AND operator has higher precedence than OR operator. So, it is evaluated first.(b and c) evaluates to false
+Now OR operator is evaluated (True or False) evaluates to True. So the if condition becomes True
+"""
 a = True
 b = False
 c = False
