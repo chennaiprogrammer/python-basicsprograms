96efb4052db88393f4fe028a01fbefb7e1b06ba0
suresh chandra sekar <sureshkavitha1997@gmail.com>
2018-11-19 20:43:34 +0530

    Add files via upload

---
+++ basic programs/print-Statements.py
@@ -0,0 +1,14 @@
+#predict the output
+#run each print statement
+
+print('abc','123')
+#print('abc','123',sep='+',sep='-')
+#print('abc',"def",sep=&,end=#)
+print('xyz','pqr',sep='@',end='$$')
+print('xab',sep='*')
+print(sep='#')
+print(int(),int(),sep='1')
+print(end="#")
+#print(sep='%',end='#',int(),float())
+#print(end='2',end='1')
+
