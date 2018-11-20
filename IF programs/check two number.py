29e4cb46c58b06695f698b5104876426d49fe23b
suresh chandra sekar <sureshkavitha1997@gmail.com>
2018-11-20 19:20:17 +0530

    Add files via upload

---
+++ IF programs/check two number.py
@@ -0,0 +1,12 @@
+
+"""enter 2 number check number even or not"""
+
+a=4
+b=6
+
+if a%2==0:
+    print('first number even number ')
+    if b%2==0:
+        print('second number is even')
+else:
+    print('second number is even')
